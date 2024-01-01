a7cc97a0f4aeb30cf4098f179c79cfde991d6caf
Griswold <dominic.pf91@gmail.com>
2024-01-02 00:50:33 +0100

    add card 11 and 16

---
+++ src/main/java/code/cards/ID_11.java
@@ -0,0 +1,48 @@
+package code.cards;
+
+import code.cards.AbstractEasyCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Iterator;
+
+import static code.ModFile.makeID;
+import static code.util.Wiz.*;
+
+public class ID_11 extends AbstractEasyCard {
+    public final static String ID = makeID("ID_11");
+    // intellij stuff ATTACK, NONE, COMMON, 3, 1, 0, 0, 0, 0
+
+    public ID_11() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.NONE);
+        baseDamage = 3;
+        baseBlock = 0;
+        baseMagicNumber = magicNumber = 0;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        int count = 0;
+        Iterator var4 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
+
+        while(var4.hasNext()) {
+            AbstractMonster m2 = (AbstractMonster)var4.next();
+            if (!m2.isDeadOrEscaped()) {
+                ++count;
+                this.addToBot(new DamageAllEnemiesAction(p, damage , damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+            }
+        }
+    }
+
+    public void upp() {
+        upgradeDamage(1);
+        upgradeBlock(0);
+        upgradeMagicNumber(0);
+        upgradeBaseCost(0);
+    }
+}
---
+++ src/main/java/code/cards/ID_16.java
@@ -0,0 +1,36 @@
+package code.cards;
+
+import code.cards.AbstractEasyCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+
+import static code.ModFile.makeID;
+import static code.util.Wiz.*;
+
+public class ID_16 extends AbstractEasyCard {
+    public final static String ID = makeID("ID_16");
+    // intellij stuff SKILL, NONE, COMMON, 0, 0, 0, 0, 1, 0
+
+    public ID_16() {
+        super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseDamage = 0;
+        baseBlock = 0;
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBot(new DiscardAction(p, p, 1+magicNumber, false));
+        this.addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, magicNumber), magicNumber));
+    }
+
+    public void upp() {
+        upgradeDamage(0);
+        upgradeBlock(0);
+        upgradeMagicNumber(0);
+        upgradeBaseCost(0);
+    }
+}
