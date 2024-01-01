d7b095cda5d0cbce48ca7a8973c5694e0103f781
Griswold <dominic.pf91@gmail.com>
2024-01-02 00:23:19 +0100

    added card 10 and 9

--- src/main/java/code/cards/ID_10.java
+++ src/main/java/code/cards/ID_10.java
@@ -1,4 +1,10 @@
+package code.cards;
+
 import code.cards.AbstractEasyCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static code.ModFile.makeID;
 import static code.util.Wiz.*;
@@ -8,17 +14,34 @@ public class ID_10 extends AbstractEasyCard {
     // intellij stuff ATTACK, ENEMY, COMMON, 7, 5, 0, 0, 0, 0
 
     public ID_10() {
-        super(ID, 2,  CardType. ATTACK, CardRarity. SPECIAL, CardTarget.  ENEMY);
-        baseDamage = 7;        baseBlock = 0;        baseMagicNumber = magicNumber = 0;    }
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = 7;
+        baseBlock = 0;
+        baseMagicNumber = magicNumber = 0;
+    }
 
-    public void use(AbstractPlayer p, AbstractMonster m) {
+    public void didDiscard() {
+        this.setCostForTurn(this.costForTurn - 1);
+    }
+    public void triggerWhenDrawn() {
+        super.triggerWhenDrawn();
+        this.setCostForTurn(this.cost - GameActionManager.totalDiscardedThisTurn);
+    }
 
+    public void atTurnStart() {
+        this.resetAttributes();
+        this.applyPowers();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_HEAVY);
     }
 
     public void upp() {
-    upgradeDamage(5);
-    upgradeBlock(0);
-    upgradeMagicNumber(0);
- upgradeBaseCost(0);
+        upgradeDamage(5);
+        upgradeBlock(0);
+        upgradeMagicNumber(0);
+        upgradeBaseCost(0);
     }
 }
--- src/main/java/code/cards/ID_9.java
+++ src/main/java/code/cards/ID_9.java
@@ -1,4 +1,10 @@
+package code.cards;
+
 import code.cards.AbstractEasyCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.watcher.MarkPower;
 
 import static code.ModFile.makeID;
 import static code.util.Wiz.*;
@@ -8,17 +14,22 @@ public class ID_9 extends AbstractEasyCard {
     // intellij stuff SKILL, PLAYER, COMMON, 0, 0, 4, 2, 1, 1
 
     public ID_9() {
-        super(ID, 0,  CardType. ATTACK, CardRarity. SPECIAL, CardTarget.  ENEMY);
-        baseDamage = 0;        baseBlock = 4;        baseMagicNumber = magicNumber = 1;    }
+        super(ID, 0, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = 0;
+        baseBlock = 4;
+        baseMagicNumber = magicNumber = 1;
+    }
 
-    public void use(AbstractPlayer p, AbstractMonster m) {
-
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        blck();
+        this.addToBot(new ApplyPowerAction(p, p, new MarkPower(p, magicNumber)));
     }
 
     public void upp() {
-    upgradeDamage(0);
-    upgradeBlock(2);
-    upgradeMagicNumber(1);
- upgradeBaseCost(0);
+        upgradeDamage(0);
+        upgradeBlock(2);
+        upgradeMagicNumber(1);
+        upgradeBaseCost(0);
     }
 }
