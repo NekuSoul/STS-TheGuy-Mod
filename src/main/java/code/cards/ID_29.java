3b375a25f35182917f4d65de32b282ad0a8e0022
Griswold <dominic.pf91@gmail.com>
2024-01-15 20:19:49 +0100

    added card 29 updated card 31

---
+++ src/main/java/code/cards/ID_29.java
@@ -0,0 +1,24 @@
+import code.cards.AbstractEasyCard;
+
+import static code.ModFile.makeID;
+import static code.util.Wiz.*;
+
+public class ID_29 extends AbstractEasyCard {
+    public final static String ID = makeID("ID_29");
+    // intellij stuff skill, self, rare, 0, 0, 0, 0, 2, 2
+
+    public ID_29() {
+        super(ID, 1,  CardType.SKILL, CardRarity.RARE , CardTarget.SELF);
+        baseDamage = 0;        baseBlock = 0;        baseMagicNumber = magicNumber = 2;    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+    }
+
+    public void upp() {
+    upgradeDamage(0);
+    upgradeBlock(0);
+    upgradeMagicNumber(2);
+ upgradeBaseCost(2);
+    }
+}
