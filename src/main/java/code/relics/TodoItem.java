2888c7963eeb7208d4c01cb12349eb1d64c43c8c
Griswold <dominic.pf91@gmail.com>
2024-01-15 21:16:44 +0100

    added starter relic stuff

--- src/main/java/code/relics/TodoItem.java
+++ src/main/java/code/relics/TodoItem.java
@@ -1,13 +1,20 @@
 package code.relics;
 
 import code.CharacterFile;
+import code.cards.ID_80;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import static code.ModFile.makeID;
 
 public class TodoItem extends AbstractEasyRelic {
     public static final String ID = makeID("TodoItem");
 
+    @Override
+    public void atBattleStart() {
+        AbstractDungeon.player.hand.addToHand(new ID_80());
+    }
     public TodoItem() {
         super(ID, RelicTier.STARTER, LandingSound.FLAT, CharacterFile.Enums.THE_GUY_COLOR);
+
     }
 }
