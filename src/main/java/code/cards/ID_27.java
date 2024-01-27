bf527dfb8a1cd5aafcb32c384159305af76e1030
Griswold <dominic.pf91@gmail.com>
2024-01-27 23:12:06 +0100

    fixed card

--- src/main/java/code/cards/ID_27.java
+++ src/main/java/code/cards/ID_27.java
@@ -1,18 +1,12 @@
 package code.cards;
 
-import code.cards.AbstractEasyCard;
 import code.powers.LambdaPower;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import static code.ModFile.makeID;
 import static code.util.Wiz.*;
@@ -33,11 +27,17 @@ public class ID_27 extends AbstractEasyCard {
         applyToSelf(new LambdaPower(makeID("ID_27_Power"), "cardStrings.EXTENDED_DESCRIPTION[0]", AbstractPower.PowerType.BUFF, false, p, magicNumber) {
 
 
-            public boolean canPlay(AbstractCard card)
+            @Override
+            public void onDrawOrDiscard()
             {
                 if(AbstractDungeon.player.hand.isEmpty())
                     atb(new DrawCardAction(1));
-                return true;
+            }
+            @Override
+            public void onAfterCardPlayed(AbstractCard usedCard)
+            {
+                if(AbstractDungeon.player.hand.isEmpty())
+                    atb(new DrawCardAction(1));
             }
 
 
